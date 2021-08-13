17f4110237ae9a0b3ad8c5b017cb1fb4062494c0
Domingo Mesa Maliniak <domingo.mm1997@gmail.com>
2021-08-13 14:09:14 +0200

    Add principle functionality

---
+++ script/index.js
@@ -0,0 +1,25 @@
+// JQUERY-like
+const $ = elem => document.querySelector(elem);
+
+/**
+ * 
+ * Variables
+ * 
+ */
+
+// Creating the elements
+    const circle = document.createElement('div');
+        circle.className = 'circle';
+    const square = document.createElement('div');
+        square.className = 'square';
+/**
+ * 
+ * Get the main element to show all the elements created on javascript.
+ * 
+ */
+    const app = $('.app');
+
+// DOM LOADED
+document.addEventListener('DOMContentLoaded', () =>{
+    const html = new Array();
+});
