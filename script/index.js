e3c817e6b40ea410a2477857444b4183550760cb
Domingo Mesa Maliniak <domingo.mm1997@gmail.com>
2021-08-16 13:28:45 +0200

    Add the view grid, and click functionality

--- script/index.js
+++ script/index.js
@@ -1,25 +1,33 @@
-// JQUERY-like
-const $ = elem => document.querySelector(elem);
 
-/**
- * 
- * Variables
- * 
- */
+
+
+const $ = elem => document.querySelector(elem);
+const $$ = elems => document.querySelectorAll(elems);
+const $app = $('.app');
 
 // Creating the elements
     const circle = document.createElement('div');
         circle.className = 'circle';
     const square = document.createElement('div');
         square.className = 'square';
-/**
- * 
- * Get the main element to show all the elements created on javascript.
- * 
- */
-    const app = $('.app');
+    const container = document.createElement('div');
+        container.className = 'container';
+
 
 // DOM LOADED
 document.addEventListener('DOMContentLoaded', () =>{
-    const html = new Array();
+    let html = ``;
+    for(let i=1; i <= 9; i++){
+        html += `<div class="block-${i} blocks"></div>`;
+    }
+    container.innerHTML = html;
+    $app.append(container);
+
+    const $$blocks = $$('.blocks');
+
+    $$blocks.forEach( (block)=>{
+        block.addEventListener('click', (e) => {
+            e.target.innerHTML = `<p>O</p>`;
+        });
+    });
 });
